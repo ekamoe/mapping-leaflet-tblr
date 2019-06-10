c91bd7418e4d6b59b19c867fa0c32493683728b9
ekamoe <kamoe@uw.edu>
2019-06-10 14:50:01 -0700

    created array of contributor data

---
+++ js/contributors.js
@@ -0,0 +1,184 @@
+const CONTRIBUTORS = [
+  {
+    name: "Nyuol Lueth Tong",
+    birthplace: "Ayeit, South Sudan",
+    rolePub: "Co-Founder / Editor-in-Chief",
+    genre: "",
+    latitude: "9.516665",
+    longitude: "30.416665"
+  },
+  {
+    name: "Maria Kuznetsova",
+    birthplace: "Kiev, Ukraine",
+    rolePub: "Fiction Editor",
+    genre: "",
+    latitude: "50.4501",
+    longitude: "30.5234"
+  },
+  {
+    name: "Novuyo Rosa Tshuma",
+    birthplace: "Zimbabwe",
+    rolePub: "Fiction Editor",
+    genre: "",
+    latitude: "-19.0154",
+    longitude: "29.1549"
+  },
+  {
+    name: "Rebecca Liu",
+    birthplace: "China",
+    rolePub: "Poetry Editor",
+    genre: "",
+    latitude: "35.8617",
+    longitude: "104.1954"
+  },
+  {
+    name: "Etan Nechin",
+    birthplace: "Israel",
+    rolePub: "Online Editor",
+    genre: "",
+    latitude: "31.0461",
+    longitude: "34.8516"
+  },
+  {
+    name: "Jenny Xie",
+    birthplace: "Hefei, China",
+    rolePub: "<i>" + "The Way of the Flesh " + "</i>",
+    genre: "(Poetry, Vol. 1)",
+    latitude: "31.8206",
+    longitude: "117.2272"
+  },
+  {
+    name: "Momina Mela",
+    birthplace: "Lahore, Pakistan",
+    rolePub: "<i>" + "This Blessed House" + "</i>",
+    genre: "Poetry, Vol. 1",
+    latitude: "31.5204",
+    longitude: "74.3587"
+  },
+  {
+    name: "Iheoma Nwachukwu",
+    birthplace: "Lagos, Nigeria",
+    rolePub: "<i>" + "Cartoline da distopia: VII. Migrant #149 & #150" + "</i>",
+    genre: "Poetry, Vol. 1",
+    latitude: "6.5244",
+    longitude: "3.3792"
+  },
+  {
+    name: "Hoa Nguyen",
+    birthplace: "Mekong Delta, Vietnam",
+    rolePub: "<i>" + "Mud Matrix" + "</i>",
+    genre: "Poetry, Vol. 1",
+    latitude: "10.0634",
+    longitude: "105.5943"
+  },
+  {
+    name: "Anca Roncea",
+    birthplace: "Romania",
+    rolePub: "<i>" + "the field let out people" + "</i>",
+    genre: "Poetry, Vol. 1",
+    latitude: "45.9432",
+    longitude: "24.9668"
+  },
+  {
+    name: "Safiya Sinclair",
+    birthplace: "Montego Bay, Jamaica",
+    rolePub:
+      "<i>" +
+      "Child of Impossibles " +
+      "</i>" +
+      "and " +
+      "<i>" +
+      "The Ragged and the Beautiful" +
+      "</i>",
+    genre: "Libretto, Vol. 1",
+    latitude: "18.4762",
+    longitude: "-77.8939"
+  },
+  {
+    name: "Ariel Dorfman",
+    birthplace: "Buenos Aires, Argentina",
+    rolePub: "<i>" + "Fair Trade" + "</i>",
+    genre: "Fiction, Vol. 1",
+    latitude: "-34.6037",
+    longitude: "-58.3816"
+  },
+  {
+    name: "Iracema Drew",
+    birthplace: "Minas Gerais, Brazil",
+    rolePub: "<i>" + "The Lost Elegies of My Late Friend Maria" + "</i>",
+    genre: "Fiction, Vol. 1",
+    latitude: "-18.5122",
+    longitude: "-44.5550"
+  },
+  {
+    name: "Horacio Castellanos Moya",
+    birthplace: "Tegucigalpa, Honduras",
+    rolePub:
+      "<i>" +
+      "False Positive " +
+      "</i>" +
+      "<br>" +
+      "(translated from the Spanish by Kelsi Vanada)",
+    genre: "Fiction, Vol. 1",
+    latitude: "14.0650",
+    longitude: "-87.1715"
+  },
+  {
+    name: "Meng Jin",
+    birthplace: "Shanghai, China",
+    rolePub: "<i>" + "Our Humans" + "</i>",
+    genre: "Fiction, Vol. 1",
+    latitude: "31.2304",
+    longitude: "121.4737"
+  },
+  {
+    name: "Dariel Suarez",
+    birthplace: "Havana, Cuba",
+    rolePub: "<i>" + "Protest" + "</i>",
+    genre: "Fiction, Vol. 1",
+    latitude: "23.1136",
+    longitude: "-82.3666"
+  },
+  {
+    name: "Nathan Go",
+    birthplace: "Philippines",
+    rolePub: "<i>" + "For Whiter Shores" + "</i>",
+    genre: "Fiction, Vol. 1",
+    latitude: "8.4961",
+    longitude: "123.3034"
+  },
+  {
+    name: "NoViolet Bulawayo",
+    birthplace: "Tsholotsho, Zimbabwe",
+    rolePub: "<i>" + "Those Ones Over There" + "</i>",
+    genre: "Fiction, Vol. 1",
+    latitude: "-19.7688",
+    longitude: "27.7579"
+  },
+  {
+    name: "Osama Alomar",
+    birthplace: "Damascus, Syria",
+    rolePub:
+      "<i>" +
+      "Wire and Steel " +
+      "</i>" +
+      "<br>" +
+      "(translated from the Arabic by C.J. Collins)",
+    genre: "Non-Fiction, Vol. 1",
+    latitude: "33.5138",
+    longitude: "36.2765"
+  },
+  {
+    name: "Hernán Díaz",
+    birthplace: "Argentina", // would like to fix to Buenos Aires for spider effect on single marker with multiple people
+    rolePub:
+      "<i>" +
+      "The Vanishing Point " +
+      "</i>" +
+      "<br>" +
+      "(a conversation with Scott Schomburg)",
+    genre: "Interview, Vol. 1",
+    latitude: "-38.4161",
+    longitude: "-63.6167"
+  }
+];
